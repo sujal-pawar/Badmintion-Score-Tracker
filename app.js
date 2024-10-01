18d759c170f0429e4e495baa91c628d4983706e3
sujal-pawar <sujalpawar00007@gmail.com>
2024-10-02 01:41:01 +0530

    initial commmit

---
+++ app.js
@@ -0,0 +1,88 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    increaseScore() {
+        this.score++;
+    }
+
+    getScore() {
+        return this.score;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    resetScore() {
+        this.score = 0;
+    }
+}
+
+class Match {
+    constructor(player1, player2, winningScore = 21, pointDifference = 2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.winningScore = winningScore;
+        this.pointDifference = pointDifference;
+    }
+
+    pointToPlayer(playerNumber) {
+        if (playerNumber === 1) {
+            this.player1.increaseScore();
+        } else if (playerNumber === 2) {
+            this.player2.increaseScore();
+        }
+        this.displayScore();
+        this.checkWinner();
+    }
+
+    checkWinner() {
+        const scoreDiff = Math.abs(this.player1.getScore() - this.player2.getScore());
+        if ((this.player1.getScore() >= this.winningScore && scoreDiff >= this.pointDifference) ||
+            (this.player2.getScore() >= this.winningScore && scoreDiff >= this.pointDifference)) {
+            this.announceWinner();
+        }
+    }
+
+    displayScore() {
+        document.getElementById('player1Score').innerText = this.player1.getScore();
+        document.getElementById('player2Score').innerText = this.player2.getScore();
+    }
+
+    announceWinner() {
+        if (this.player1.getScore() > this.player2.getScore()) {
+            alert(`${this.player1.getName()} wins the match!`);
+        } else {
+            alert(`${this.player2.getName()} wins the match!`);
+        }
+    }
+
+    resetMatch() {
+        this.player1.resetScore();
+        this.player2.resetScore();
+        this.displayScore();
+    }
+}
+
+// Initialize game
+let player1Name = '';
+let player2Name = '';
+
+document.getElementById('player1Name').addEventListener('input', function() {
+    player1Name = this.value;
+    document.getElementById('player1DisplayName').innerText = player1Name || 'Player 1';
+});
+
+document.getElementById('player2Name').addEventListener('input', function() {
+    player2Name = this.value;
+    document.getElementById('player2DisplayName').innerText = player2Name || 'Player 2';
+});
+
+let match = new Match(new Player('Player 1'), new Player('Player 2'));
+
+document.getElementById('reset').addEventListener('click', () => {
+    match.resetMatch();
+});
